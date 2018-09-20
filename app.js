d1c182851df018d29db16c0fd4a734be7a5fc037
Bob Polacheck <rpolacheck@gmail.com>
2018-09-20 14:24:47 +0000

    Added new route for show page

--- app.js
+++ app.js
@@ -14,24 +14,26 @@ app.set("view engine", "ejs");
 
 var campgroundSchema = new mongoose.Schema({
    name: String,
-   image: String
+   image: String,
+   description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
 
-// Campground.create(
-//     {
-//         name: "Granite Hill", 
-//         image: "https://images.unsplash.com/photo-1533575770077-052fa2c609fc?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ebb0b2d80f4b2d82f3587492b80e2321&auto=format&fit=crop&w=500&q=60"
+Campground.create(
+    {
+        name: "Granite Hill", 
+        image: "https://images.unsplash.com/photo-1533575770077-052fa2c609fc?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ebb0b2d80f4b2d82f3587492b80e2321&auto=format&fit=crop&w=500&q=60",
+        description: "This is a huge granite hill, no water or bathrooms"
     
-//     }, function(err, campground){
-//       if(err){
-//           console.log(err);
-//       } else {
-//           console.log("Newly Created Campground");
-//           console.log(campground);
-//       }
-//     });
+    }, function(err, campground){
+      if(err){
+          console.log(err);
+      } else {
+          console.log("Newly Created Campground");
+          console.log(campground);
+      }
+    });
 
 
 app.get("/", function(req,res){
@@ -67,8 +69,15 @@ app.post("/campgrounds", function(req, res){
   
 });
 
+// Create a New Campground
 app.get("/campgrounds/new", function(req, res){
    res.render("new.ejs"); 
+});
+
+app.get("/campgrounds/:id", function(req, res){
+    //find the campground with provided ID
+    //render show template with that campground
+   res.send("This will be the SHOW PAGE one day!"); 
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
