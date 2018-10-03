495475f11a50a6af72fae6b36a4d8b569bb2b8ba
Bob Polacheck <rpolacheck@gmail.com>
2018-10-03 17:51:55 +0000

    Added comment module and added to show page

--- app.js
+++ app.js
@@ -12,28 +12,6 @@ mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-
-
-// Schema Setup
-
-
-
-// Campground.create(
-//     {
-//         name: "Granite Hill", 
-//         image: "https://images.unsplash.com/photo-1533575770077-052fa2c609fc?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ebb0b2d80f4b2d82f3587492b80e2321&auto=format&fit=crop&w=500&q=60",
-//         description: "This is a huge granite hill, no water or bathrooms"
-    
-//     }, function(err, campground){
-//       if(err){
-//           console.log(err);
-//       } else {
-//           console.log("Newly Created Campground");
-//           console.log(campground);
-//       }
-//     });
-
-
 app.get("/", function(req,res){
    res.render("landing");
 });
@@ -76,10 +54,11 @@ app.get("/campgrounds/new", function(req, res){
 //SHOW - shows more info about one campground
 app.get("/campgrounds/:id", function(req, res){
     //find the campground with provided ID
-    Campground.findById(req.params.id, function(err, foundCampground){
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
        if(err){
            console.log(err);
        } else {
+           console.log(foundCampground);
            //render show template with that campground
             res.render("show", {campground: foundCampground});
        }
