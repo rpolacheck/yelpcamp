deee82a1d87d723851772e77341c096ecaf4687f
Bob Polacheck <rpolacheck@gmail.com>
2018-10-06 01:29:56 +0000

    added passport and registration page

--- app.js
+++ app.js
@@ -18,6 +18,20 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname+ "/public"));
 seedDB();
 
+
+//PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "I just doesn't matter",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.get("/", function(req,res){
    res.render("landing");
 });
@@ -112,6 +126,29 @@ app.post("/campgrounds/:id/comments", function(req, res){
     // create new comment
     //connect new comment to campground
     //redirect to showpage of campground
+});
+
+/// ===========
+/// AUTH ROUTES
+/// ===========
+
+//show register form
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+// Handle sign up logic
+app.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username});
+   User.register(newUser, req.body.password, function(err, user){
+      if(err){
+          console.log(err);
+          return res.render("register");
+      } 
+      passport.authenticate("local")(req, res, function(){
+         res.redirect("/campgrounds"); 
+      });
+   });
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
