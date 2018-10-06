8faad0167a718a7632a24da69d3e6836fc8ff47c
Bob Polacheck <rpolacheck@gmail.com>
2018-10-06 23:34:52 +0000

    Cannot get functionality in the redirect nonuser edit button

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -66,13 +66,9 @@ router.get("/:id", function(req, res){
 });
 
 // Edit Campground Route
-router.get("/:id/edit", function(req, res){
+router.get("/:id/edit", checkCampgroundOwnership, function(req, res){
     Campground.findById(req.params.id, function(err, foundCampground){
-       if(err){
-           res.redirect("/campgrounds")
-       } else {
-           res.render("campgrounds/edit", {campground: foundCampground});
-       }
+        res.render("campgrounds/edit", {campground: foundCampground});
     });
 });
 
@@ -108,6 +104,26 @@ function isLoggedIn(req, res, next){
         return next();
     }
     res.redirect("/login");
+}
+
+function checkCampgroundOwnership(req, res, next) {
+    if(req.isAuthenticated()){
+        
+        Campground.findById(req.params.id, function(err, foundCampground){
+           if(err){
+               res.redirect("/campgrounds")
+           } else {
+               //does user own the campground?
+               if(foundCampground.author.id.equals(req.user._id)) {
+                   next();
+               } else {
+                   res.redirect("back");
+               }
+           }
+        });
+    } else {
+        res.redirect("back");
+    }
 }
 
 module.exports = router;
